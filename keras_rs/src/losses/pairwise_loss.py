cde38689f0d27d0570a6745d24b43d8e1061605d
Abheesht Sharma <abheesht@google.com>
2025-02-25 23:29:52 +0530

    Add doc-strings

--- keras_rs/src/losses/pairwise_loss.py
+++ keras_rs/src/losses/pairwise_loss.py
@@ -6,9 +6,22 @@ from keras import ops
 
 from keras_rs.src import types
 from keras_rs.src.utils.loss_utils import pairwise_comparison
+from keras_rs.src.utils.loss_utils import process_loss_call_inputs
 
 
 class PairwiseLoss(keras.losses.Loss):
+    """Base class for pairwise ranking losses.
+
+    Pairwise loss functions are designed for ranking tasks, where the goal is to
+    correctly order items within each list. Any pairwise loss function computes
+    the loss value by comparing pairs of items within each list, penalizing
+    cases where an item with a higher true label has a lower predicted score
+    than an item with a lower true label.
+
+    In order to implement any kind of pairwise loss, override the
+    `pairwise_loss` method.
+    """
+
     # TODO: Add `temperature`, `lambda_weights`.
 
     @abc.abstractmethod
@@ -37,11 +50,39 @@ class PairwiseLoss(keras.losses.Loss):
 
         return self.pairwise_loss(pairwise_logits), pairwise_labels
 
-    def call(
+    def __call__(
         self,
         y_true: types.Tensor,
         y_pred: types.Tensor,
         sample_weight: Optional[types.Tensor] = None,
+    ) -> types.Tensor:
+        """
+        Args:
+            y_true: tensor. Ground truth values, of shape `(list_size)` for
+                unbatched inputs or `(batch_size, list_size)` for batched
+                inputs.
+            y_pred: tensor. The predicted values, of shape `(list_size)` for
+                unbatched inputs or `(batch_size, list_size)` for batched
+                inputs. Should be of the same shape as `y_true`.
+            sample_weight: tensor. Optional sample weight acts as reduction
+                weighting coefficient for the per-sample losses. If a scalar is
+                provided, then the loss is simply scaled by the given value. If
+                `sample_weight` is a tensor of size `(batch_size)`, then the
+                total loss for each sample of the batch is rescaled by the
+                corresponding element in the `sample_weight` vector.
+                If the shape of sample_weight is the same as `y_true`, i.e,
+                item-wise sample weight, then each item of `y_pred` is scaled by
+                the corresponding value of `sample_weight`.
+        """
+        y_true, y_pred, sample_weight = process_loss_call_inputs(
+            y_true, y_pred, sample_weight
+        )
+        super().__call__(y_true, y_pred, sample_weight)
+
+    def call(
+        self,
+        y_true: types.Tensor,
+        y_pred: types.Tensor,
     ) -> types.Tensor:
         mask = None
         if isinstance(y_true, dict):
--- keras_rs/src/utils/loss_utils.py
+++ keras_rs/src/utils/loss_utils.py
@@ -1,6 +1,9 @@
+from typing import Optional
+
 from keras import ops
 
 from keras_rs.src import types
+from keras_rs.src.utils.keras_utils import check_shapes_compatible
 
 
 def apply_pairwise_op(x: types.Tensor, op: ops) -> types.Tensor:
@@ -33,3 +36,71 @@ def pairwise_comparison(
         )
 
     return pairwise_labels, pairwise_logits
+
+
+def process_loss_call_inputs(
+    y_true: types.Tensor,
+    y_pred: types.Tensor,
+    sample_weight: Optional[types.Tensor] = None,
+) -> tuple[types.Tensor, types.Tensor, types.Tensor]:
+    """
+    This utility function does three things:
+
+    - Checks that `y_true`, `y_pred` are of rank 1 or 2. `sample_weight` can be
+      of ranks 0, 1, 2.
+    - Check that `y_true` and `y_pred` have the same shape.
+    - Add batch dimension if rank = 1.
+    """
+
+    def get_shape(
+        x: types.Tensor, convert_to_tensor: bool = False
+    ) -> tuple[types.Tensor, types.TensorShape, int]:
+        if convert_to_tensor and isinstance(x, (list, float, int)):
+            x = ops.array(x)
+        shape = ops.shape(x)
+        rank = len(shape)
+        return x, shape, rank
+
+    # These are typecast to tensors in `keras.losses.Loss.__call__()`. So,
+    # not need to convert them to tensors.
+    y_true, y_true_shape, y_true_rank = get_shape(y_true)
+    y_pred, y_pred_shape, y_pred_rank = get_shape(y_pred)
+    # `keras.losses.Loss.__call__()` does not convert `sample_weight` to tensor.
+    if sample_weight is not None:
+        sample_weight, sample_weight_shape, sample_weight_rank = get_shape(
+            sample_weight, convert_to_tensor=True
+        )
+
+    # Check ranks and shapes.
+    def check_rank(
+        x_rank: int,
+        allowed_ranks: tuple[int, ...] = (1, 2),
+        tensor_name: Optional[str] = None,
+    ) -> None:
+        if x_rank not in allowed_ranks:
+            raise ValueError(
+                f"`{tensor_name}` should have a rank from `{allowed_ranks}`."
+                f"Received: `{x_rank}`."
+            )
+
+    check_rank(y_true_rank, tensor_name="y_true")
+    check_rank(y_pred_rank, tensor_name="y_pred")
+    if not check_shapes_compatible(y_true_shape, y_pred_shape):
+        raise ValueError(
+            "`y_true` and `y_pred` should have the same shape. Received: "
+            f"`y_true.shape` = {y_true_shape}, `y_pred.shape` = {y_pred_shape}."
+        )
+    if sample_weight is not None:
+        check_rank(
+            sample_weight_rank,
+            allowed_ranks=tuple(range(y_true_rank + 1)),
+            tensor_name="sample_weight",
+        )
+
+    if y_true_rank == 1:
+        # Do not need to modify sample_weight. `keras.losses.Loss` takes care of
+        # it.
+        y_true = ops.expand_dims(y_true, axis=0)
+        y_pred = ops.expand_dims(y_pred, axis=0)
+
+    return y_true, y_pred, sample_weight
