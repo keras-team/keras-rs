7981dc7ea660ba8fc63ff1299601b8faf0687be5
Abheesht Sharma <abheesht@google.com>
2025-04-20 21:04:15 +0530

    Add MAP

---
+++ keras_rs/src/metrics/mean_average_precision.py
@@ -0,0 +1,111 @@
+from keras import ops
+
+from keras_rs.src import types
+from keras_rs.src.api_export import keras_rs_export
+from keras_rs.src.metrics.ranking_metric import RankingMetric
+from keras_rs.src.metrics.ranking_metric import (
+    ranking_metric_subclass_doc_string,
+)
+from keras_rs.src.metrics.ranking_metric import (
+    ranking_metric_subclass_doc_string_args,
+)
+from keras_rs.src.metrics.ranking_metrics_utils import get_list_weights
+from keras_rs.src.metrics.ranking_metrics_utils import sort_by_scores
+from keras_rs.src.utils.doc_string_utils import format_docstring
+
+
+@keras_rs_export("keras_rs.metrics.MeanAveragePrecision")
+class MeanAveragePrecision(RankingMetric):
+    def compute_metric(
+        self,
+        y_true: types.Tensor,
+        y_pred: types.Tensor,
+        mask: types.Tensor,
+        sample_weight: types.Tensor,
+    ) -> types.Tensor:
+        relevance = ops.cast(
+            ops.greater_equal(y_true, ops.cast(1, dtype=y_true.dtype)),
+            dtype="float32",
+        )
+        sorted_relevance, sorted_weights = sort_by_scores(
+            tensors_to_sort=[relevance, sample_weight],
+            scores=y_pred,
+            mask=mask,
+            k=self.k,
+            shuffle_ties=self.shuffle_ties,
+            seed=self.seed_generator,
+        )
+        per_list_relevant_counts = ops.cumsum(sorted_relevance, axis=1)
+        per_list_cutoffs = ops.cumsum(ops.ones_like(sorted_relevance), axis=1)
+        per_list_precisions = ops.divide_no_nan(
+            per_list_relevant_counts, per_list_cutoffs
+        )
+
+        total_precision = ops.sum(
+            ops.multiply(
+                per_list_precisions,
+                ops.multiply(sorted_weights, sorted_relevance),
+            ),
+            axis=1,
+            keepdims=True,
+        )
+
+        # Compute the total relevance.
+        total_relevance = ops.sum(
+            ops.multiply(sample_weight, relevance), axis=1, keepdims=True
+        )
+
+        per_list_map = ops.divide_no_nan(total_precision, total_relevance)
+
+        per_list_weights = get_list_weights(sample_weight, relevance)
+
+        return per_list_map, per_list_weights
+
+
+concept_sentence = (
+    "an average of precision values computed after each relevant item is "
+    "encountered in the ranked list"
+)
+relevance_type = "binary indicators (0 or 1) of relevance"
+score_range_interpretation = (
+    "from 0 to 1, with higher values indicating that relevant items are "
+    "generally positioned higher in the ranking"
+)
+
+formula = """
+    ```
+    MAP(y, s) = sum_k (P@k(y, s) * rel(k)) / sum_i y_i
+    rel(k) = y_i if rank(s_i) = k
+    ```
+
+    where:
+        - `k` represents the rank position (starting from 1).
+        - `sum_k` indicates a summation over all ranks `k` from 1 up to the list
+          size (or the specified cutoff).
+        - `P@k(y, s)` denotes the Precision at rank `k`, calculated as the
+          number of relevant items found within the top `k` positions divided by
+          `k`.
+        - `rel(k)` represents the relevance of the item specifically at rank
+          `k`. `rel(k)` is 1 if the item at rank `k` is relevant, and 0
+          otherwise.
+        - `y_i` is the true relevance label of the original item `i` before
+          ranking.
+        - `rank(s_i)` is the rank position assigned to item `i` based on its
+          score `s_i`.
+        - `sum_i y_i` calculates the total number of relevant items in the
+          original list `y`.
+        - If `sum_i y_i` (the total number of relevant items) is 0, the score
+          for this query (Average Precision) is defined as 0.
+"""
+extra_args = ""
+
+MeanAveragePrecision.__doc__ = format_docstring(
+    ranking_metric_subclass_doc_string,
+    width=80,
+    metric_name="Mean Average Precision",
+    metric_abbreviation="MAP",
+    concept_sentence=concept_sentence,
+    relevance_type=relevance_type,
+    score_range_interpretation=score_range_interpretation,
+    formula=formula,
+) + ranking_metric_subclass_doc_string_args.format(extra_args=extra_args)
--- keras_rs/src/metrics/mean_average_precision_test.py
+++ keras_rs/src/metrics/mean_average_precision_test.py
@@ -5,26 +5,26 @@ from keras.metrics import deserialize
 from keras.metrics import serialize
 
 from keras_rs.src import testing
-from keras_rs.src.metrics.mean_reciprocal_rank import MeanReciprocalRank
+from keras_rs.src.metrics.mean_average_precision import MeanAveragePrecision
 
 
-class MeanReciprocalRankTest(testing.TestCase, parameterized.TestCase):
+class MeanAveragePrecisionTest(testing.TestCase, parameterized.TestCase):
     def setUp(self):
         self.y_true_batched = ops.array(
             [
                 [0, 0, 1, 0],
-                [0, 3, 0, 0],  # Rank 2 -> MRR = 0.5
-                [0, 0, 0, 0],  # Rank N/A -> MRR = 0.0
-                [1, 0, 2, 0],  # Rank 1 (first) -> MRR = 1.0
+                [0, 3, 4, 0],
+                [0, 0, 0, 0],
+                [1, 0, 2, 0],
             ],
             dtype="float32",
         )
         self.y_pred_batched = ops.array(
             [
-                [0.1, 0.2, 0.9, 0.3],  # MRR = 1.0
-                [0.8, 0.7, 0.1, 0.2],  # MRR = 0.5
-                [0.4, 0.3, 0.2, 0.1],  # MRR = 0.0
-                [0.9, 0.2, 0.8, 0.3],  # MRR = 1.0
+                [0.1, 0.2, 0.9, 0.3],
+                [0.8, 0.7, 0.1, 0.2],
+                [0.4, 0.3, 0.2, 0.1],
+                [0.9, 0.2, 0.1, 0.3],
             ],
             dtype="float32",
         )
@@ -33,15 +33,15 @@ class MeanReciprocalRankTest(testing.TestCase, parameterized.TestCase):
         with self.assertRaisesRegex(
             ValueError, "`k` should be a positive integer"
         ):
-            MeanReciprocalRank(k=0)
+            MeanAveragePrecision(k=0)
         with self.assertRaisesRegex(
             ValueError, "`k` should be a positive integer"
         ):
-            MeanReciprocalRank(k=-5)
+            MeanAveragePrecision(k=-5)
         with self.assertRaisesRegex(
             ValueError, "`k` should be a positive integer"
         ):
-            MeanReciprocalRank(k=3.5)  # type: ignore
+            MeanAveragePrecision(k=3.5)
 
     @parameterized.named_parameters(
         (
@@ -74,10 +74,10 @@ class MeanReciprocalRankTest(testing.TestCase, parameterized.TestCase):
         ),
         (
             "multiple_relevant_items",
-            [1.0, 0.0, 1.0, 0.0],
-            [0.9, 0.2, 0.8, 0.3],
+            [1.0, 0.0, 2.0, 0.0],
+            [0.9, 0.2, 0.1, 0.3],
             None,
-            1.0,
+            0.75,
         ),
         (
             "sample_weight_0",
@@ -96,38 +96,38 @@ class MeanReciprocalRankTest(testing.TestCase, parameterized.TestCase):
         (
             "sample_weight_1d",
             [1.0, 0.0, 1.0, 0.0],
-            [0.9, 0.2, 0.8, 0.3],
+            [0.9, 0.2, 0.1, 0.3],
             [2.0, 1.0, 3.0, 0.0],
-            1.0,
+            0.8,
         ),
     )
     def test_unbatched_inputs(
         self, y_true, y_pred, sample_weight, expected_output
     ):
-        mrr_metric = MeanReciprocalRank()
-        mrr_metric.update_state(y_true, y_pred, sample_weight=sample_weight)
-        result = mrr_metric.result()
+        map_metric = MeanAveragePrecision()
+        map_metric.update_state(y_true, y_pred, sample_weight=sample_weight)
+        result = map_metric.result()
         self.assertAllClose(result, expected_output)
 
     def test_batched_input(self):
-        mrr_metric = MeanReciprocalRank()
-        mrr_metric.update_state(self.y_true_batched, self.y_pred_batched)
-        result = mrr_metric.result()
-        self.assertAllClose(result, 0.625)
+        map_metric = MeanAveragePrecision()
+        map_metric.update_state(self.y_true_batched, self.y_pred_batched)
+        result = map_metric.result()
+        self.assertAllClose(result, 0.5625)
 
     @parameterized.named_parameters(
-        ("scalar_0.5", 0.5, 0.625),
+        ("scalar_0.5", 0.5, 0.5625),
         ("scalar_0", 0, 0),
-        ("scalar_1d", [1.0, 0.5, 2.0, 1.0], 0.675),
+        ("1d", [1.0, 0.5, 2.0, 1.0], 0.6),
     )
     def test_batched_inputs_sample_weight(self, sample_weight, expected_output):
-        mrr_metric = MeanReciprocalRank()
-        mrr_metric.update_state(
+        map_metric = MeanAveragePrecision()
+        map_metric.update_state(
             self.y_true_batched,
             self.y_pred_batched,
             sample_weight=sample_weight,
         )
-        result = mrr_metric.result()
+        result = map_metric.result()
         self.assertAllClose(result, expected_output)
 
     @parameterized.named_parameters(
@@ -154,19 +154,19 @@ class MeanReciprocalRankTest(testing.TestCase, parameterized.TestCase):
         ),
         (
             "general_case",
-            [[0, 1, 0, 0], [1, 0, 0, 1]],
+            [[0, 1, 1, 0], [1, 0, 2, 1]],
             [[0.8, 0.7, 0.1, 0.2], [0.9, 0.1, 0.2, 0.3]],
-            [[0.8, 0.8, 0.0, 0.0], [0.0, 0.0, 1.0, 1.0]],
-            0.777778,
+            [[0.8, 1.0, 1.0, 0.0], [1.0, 1.0, 0.0, 0.0]],
+            0.791667,
         ),
     )
     def test_2d_sample_weight(
         self, y_true, y_pred, sample_weight, expected_output
     ):
-        mrr_metric = MeanReciprocalRank()
+        map_metric = MeanAveragePrecision()
 
-        mrr_metric.update_state(y_true, y_pred, sample_weight=sample_weight)
-        result = mrr_metric.result()
+        map_metric.update_state(y_true, y_pred, sample_weight=sample_weight)
+        result = map_metric.result()
         self.assertAllClose(result, expected_output)
 
     @parameterized.named_parameters(
@@ -194,56 +194,84 @@ class MeanReciprocalRankTest(testing.TestCase, parameterized.TestCase):
         (
             "general_case",
             {
-                "labels": [[0, 1, 0, 0], [1, 0, 0, 1]],
+                "labels": [[0, 1, 1, 0], [1, 0, 2, 1]],
                 "mask": [
+                    [True, True, True, False],
                     [True, True, False, False],
-                    [False, False, True, True],
                 ],
             },
             [[0.8, 0.7, 0.1, 0.2], [0.9, 0.1, 0.2, 0.3]],
             [[0.8, 0.8, 1.0, 1.0], [0.0, 0.0, 1.0, 1.0]],
-            0.777778,
+            0.592593,
         ),
     )
     def test_masking(self, y_true, y_pred, sample_weight, expected_output):
-        mrr_metric = MeanReciprocalRank()
+        map_metric = MeanAveragePrecision()
 
-        mrr_metric.update_state(y_true, y_pred, sample_weight=sample_weight)
-        result = mrr_metric.result()
+        map_metric.update_state(y_true, y_pred, sample_weight=sample_weight)
+        result = map_metric.result()
         self.assertAllClose(result, expected_output)
 
     @parameterized.named_parameters(
-        ("1", 1, 0.5), ("2", 2, 0.625), ("3", 3, 0.625), ("4", 4, 0.625)
+        ("1", 1, 0.375),
+        ("2", 2, 0.4375),
+        ("3", 3, 0.4375),
+        ("4", 4, 0.5625),
     )
-    def test_k(self, k, expected_mrr):
-        mrr_metric = MeanReciprocalRank(k=k)
-        mrr_metric.update_state(self.y_true_batched, self.y_pred_batched)
-        result = mrr_metric.result()
-        self.assertAllClose(result, expected_mrr)
+    def test_k(self, k, expected_map):
+        map_metric = MeanAveragePrecision(k=k)
+        map_metric.update_state(self.y_true_batched, self.y_pred_batched)
+        result = map_metric.result()
+        self.assertAllClose(result, expected_map)
 
     def test_statefulness(self):
-        mrr_metric = MeanReciprocalRank()
+        map_metric = MeanAveragePrecision()
         # Batch 1: First two lists
-        mrr_metric.update_state(
+        map_metric.update_state(
             self.y_true_batched[:2], self.y_pred_batched[:2]
         )
-        result = mrr_metric.result()
+        result = map_metric.result()
         self.assertAllClose(result, 0.75)
 
         # Batch 2: Last two lists
-        mrr_metric.update_state(
+        map_metric.update_state(
             self.y_true_batched[2:], self.y_pred_batched[2:]
         )
-        result = mrr_metric.result()
-        self.assertAllClose(result, 0.625)
+        result = map_metric.result()
+        self.assertAllClose(result, 0.5625)
 
         # Reset state
-        mrr_metric.reset_state()
-        result = mrr_metric.result()
+        map_metric.reset_state()
+        result = map_metric.result()
         self.assertAllClose(result, 0.0)
 
+    @parameterized.named_parameters(
+        ("0.5", 0.5, 0.5625),
+        ("weight_0", 0.0, 0.0),
+    )
+    def test_scalar_sample_weight(self, sample_weight, expected_output):
+        map_metric = MeanAveragePrecision()
+        map_metric.update_state(
+            self.y_true_batched,
+            self.y_pred_batched,
+            sample_weight=sample_weight,
+        )
+        result = map_metric.result()
+        self.assertAllClose(result, expected_output)
+
+    def test_1d_sample_weight(self):
+        map_metric = MeanAveragePrecision()
+        sample_weight = ops.array([1.0, 0.5, 2.0, 1.0], dtype="float32")
+        map_metric.update_state(
+            self.y_true_batched,
+            self.y_pred_batched,
+            sample_weight=sample_weight,
+        )
+        result = map_metric.result()
+        self.assertAllClose(result, 0.6)
+
     def test_serialization(self):
-        metric = MeanReciprocalRank()
+        metric = MeanAveragePrecision()
         restored = deserialize(serialize(metric))
         self.assertDictEqual(metric.get_config(), restored.get_config())
 
@@ -254,7 +282,7 @@ class MeanReciprocalRankTest(testing.TestCase, parameterized.TestCase):
 
         model.compile(
             loss=keras.losses.MeanSquaredError(),
-            metrics=[MeanReciprocalRank()],
+            metrics=[MeanAveragePrecision()],
             optimizer="adam",
         )
         model.evaluate(
