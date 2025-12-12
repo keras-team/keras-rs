371488d03b6ee56f1ec30f0365347eaf21fcdf1d
Abheesht Sharma <abheesht@google.com>
2025-12-12 13:12:35 +0530

    Pass correct profile path

--- examples/ml_perf/main.py
+++ examples/ml_perf/main.py
@@ -278,7 +278,7 @@ def main(
         options.python_tracer_level = 1
         options.host_tracer_level = os.getenv("HOST_TRACER_LEVEL", 1)
         jax.profiler.start_trace(
-            training_config.profile_log_path,
+            training_cfg.profile_log_path,
             profiler_options=options
         )
     model.fit(
